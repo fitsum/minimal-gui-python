7aaaecba5d1556d0b5d88fd044826e954e6d4699
Fitsum <fitsumbelay@gmail.com>
2018-02-24 18:36:48 -0500

    :wrench: moved settings to init

--- app.sh
+++ app.sh
@@ -6,13 +6,18 @@ class Application(tk.Frame):
         tk.Frame.__init__(self, master)
         self.grid()
         self.createWidgets()
+        self.width = self.winfo_reqwidth()/2
+        self.master.title('Sample application')
+        self.master.geometry("400x300")
+        self.master.resizable()
 
     def createWidgets(self):
         self.quitButton = tk.Button(self, text='Quit',
             command=self.quit)
         self.quitButton.grid()
 
-app = Application()
-app.master.title('Sample application')
-app.mainloop()                            
 
+app = Application()
+app.mainloop() 
+
+                           
