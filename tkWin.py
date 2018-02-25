b69f1b662af22f0d55b5c6d80dcb4ea1bf9b914e
Fitsum <fitsumbelay@gmail.com>
2018-02-24 20:24:46 -0500

    :boom::hammer: unswitched files, actually saved module this time with OK name

---
+++ tkWin.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import Tkinter as tk
+
+class App(tk.Frame):
+	def __init__(self, master=None):
+		tk.Frame.__init__(self, master)
+		self.grid()
+		self.createWidgets()
+		self.applySettings()
+		self.mainloop()
+
+	def createWidgets(self):
+		self.quitButton = tk.Button(self, text='Quit',
+            command=self.quit)
+		self.quitButton.grid()
+
+	def applySettings(self):
+		self.master.title('App')
+		self.master.geometry("400x300")
+		self.master.resizable()
+		#self.master.wm_overrideredirect(True)
+
+
+                           
