b86141756afbef2d26269db7eb303ab59fb17594
Fitsum <fitsumbelay@gmail.com>
2018-02-24 19:26:48 -0500

    :wrench::boom: added applySettings method, renamed app, app fires on init

--- app.sh
+++ app.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env python
 import Tkinter as tk
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        tk.Frame.__init__(self, master)
-        self.grid()
-        self.createWidgets()
-        self.width = self.winfo_reqwidth()/2
-        self.master.title('Sample application')
-        self.master.geometry("400x300")
-        self.master.resizable()
+class App(tk.Frame):
+	def __init__(self, master=None):
+		tk.Frame.__init__(self, master)
+		self.grid()
+		self.createWidgets()
+		self.applySettings()
+		self.mainloop()
 
-    def createWidgets(self):
-        self.quitButton = tk.Button(self, text='Quit',
+	def createWidgets(self):
+		self.quitButton = tk.Button(self, text='Quit',
             command=self.quit)
-        self.quitButton.grid()
+		self.quitButton.grid()
 
+	def applySettings(self):
+		self.master.title('App')
+		self.master.geometry("400x300")
+		self.master.resizable()
+		#self.master.wm_overrideredirect(True)
 
-app = Application()
-app.mainloop() 
+app = App()
 
                            
